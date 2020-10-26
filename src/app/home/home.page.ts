46b07f13be075db1ea221dba08e5c8c0276e3980
spenalva1 <penalvasantiagogm@gmail.com>
2020-10-26 18:42:51 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  playersForm: FormGroup;
+  quantity = 2;
 
   constructor() {}
+
+  ngOnInit() {
+    this.createForm(2);
+  }
+
+  onChangeQuantity(){
+    this.createForm(this.playersForm.value.quantity);
+  }
+
+  createForm(quantity: number){
+    this.playersForm = new FormGroup({
+      quantity: new FormControl(quantity)
+    });
+    for (let i = 0; i < quantity; i++){
+      this.playersForm.addControl(`player${i}`, new FormControl(null));
+    }
+    this.quantity = quantity;
+  }
+
+  onPlay(){
+    console.log(this.playersForm.value);
+  }
+
+
 
 }
